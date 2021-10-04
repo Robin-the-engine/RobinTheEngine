0f6b3bdbf19e1db17a1b8be4147547576f10439f
Stas <stastmn@gmail.com>
2021-10-04 23:37:38 +0300

    Fix dir diferences

--- RobinTheEngine/src/RobinTheEngine/ImGui/ImGuiBuild.cpp
+++ RobinTheEngine/src/RobinTheEngine/ImGui/ImGuiBuild.cpp
@@ -1,7 +1,7 @@
 #include "rtepch.h"
 
 //#include "examples/imgui_impl_dx12.cpp"
-#include "examples/imgui_impl_dx11.cpp"
+#include "backends/imgui_impl_dx11.cpp"
 //#include "examples/imgui_impl_win32.cpp"
-#include "examples/imgui_impl_glfw.cpp"
-//#include "examples/imgui_impl_win32.cpp"
+#include "backends/imgui_impl_glfw.cpp"
+//#include "backends/imgui_impl_win32.cpp"
