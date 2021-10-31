3da2525e773fea3bbacdb194b111eafc9130c95e
Alexander Sukhikh <alexsm.rf24@gmail.com>
2021-11-01 02:07:33 +0300

    Scripting fixes

--- RobinTheEngine/src/RobinTheEngine/Scene/Components/Scripting.cpp
+++ RobinTheEngine/src/RobinTheEngine/Scene/Components/Scripting.cpp
@@ -9,20 +9,54 @@ ScriptComponent::ScriptComponent() :
     lua.open_libraries(sol::lib::base);
 }
 
-void ScriptComponent::attachScript(const std::string& script, bool isFile) {
+bool ScriptComponent::checkScript() {
+    return scriptAttached;
+}
+
+void ScriptComponent::onAttachEnd(const std::string& script, bool isFile) {
+    this->isFile = isFile;
+    this->script = script;
+    scriptAttached = true;
+}
+
+void ScriptComponent::onAttachStart(const std::string& script, bool isFile) {
     if (isFile) {
         executable = lua.load_file(script);
     }
     else {
         executable = lua.load(script);
     }
-    if (executable.valid()) {
-        scriptAttached = true;
-    }
 }
 
-void ScriptComponent::reloadScript() {
-    attachScript(script, isFile);
+bool ScriptComponent::attachScript(const std::string& script, bool isFile) {
+    // Note: script will be executed once on the load
+    // to fill function table inside the sol2 library
+    // if you want to execute the script manually use
+    onAttachStart(script, isFile);
+    if (executable.valid()) {
+        onAttachEnd(script, isFile);
+        call();
+        return true;
+    }
+    return false;
+}
+
+bool RTE::ScriptComponent::attachScriptNoExec(const std::string& script, bool isFile) {
+    // Note: script will be only loaded, but not executed, and,
+    // therefore function table inside the sol2 library will not be filled
+    // so, you have to execute the script manually before use
+    onAttachStart(script, isFile);
+    if (executable.valid()) {
+        onAttachEnd(script, isFile);
+        return true;
+    }
+    return false;
+}
+
+bool ScriptComponent::reloadScript() {
+    if (scriptAttached) {
+        return attachScript(script, isFile);
+    }
 }
 
 sol::state& ScriptComponent::getState() {
--- RobinTheEngine/src/RobinTheEngine/Scene/Components/Scripting.h
+++ RobinTheEngine/src/RobinTheEngine/Scene/Components/Scripting.h
@@ -9,30 +9,36 @@ namespace RTE {
     class ScriptComponent {
     public:
         virtual ~ScriptComponent() = default;
-        virtual void attachScript(const std::string& script, bool isFile = true);
-        virtual void reloadScript();
+        virtual bool attachScript(const std::string& script, bool isFile = true);
+        virtual bool attachScriptNoExec(const std::string& script, bool isFile = true);
+        virtual bool reloadScript();
         virtual sol::state& getState();
 
         template<typename ...Params>
-        void call(Params ...) {
-            if (scriptAttached) {
-                executable(Params ...);
+        void call(Params ...args) {
+            if (!checkScript()) {
+                return;
             }
+            executable(std::forward<Params>(args)...);
         }
 
-        template<>
-        void call(float x, float y, float z) {
-            if (scriptAttached) {
-                executable(x, y, z);
+        template<typename ...Params>
+        void callf(std::string func, Params ...args) {
+            if (!checkScript()) {
+                return;
             }
+            lua[func](std::forward<Params>(args)...);
         }
 
     protected:
         ScriptComponent();
+        virtual bool checkScript();
+        virtual void onAttachEnd(const std::string& script, bool isFile = true);
+        virtual void onAttachStart(const std::string& script, bool isFile = true);
 
     private:
         sol::state lua;
-        const std::string& script;
+        std::string script;
         sol::load_result executable;
         bool isFile = true;
         bool scriptAttached = false;
@@ -40,7 +46,7 @@ namespace RTE {
 
     template<typename UserType>
     void registerUserType() {
-        assert("you must create specializetion of this template for the type you need" && false);
+        static_assert("you must create specializetion of this template for the type you need" && false);
     }
 
 }
