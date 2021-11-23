3097d424360f7090291929dd4dcc75db0f435f3f
Sukhikh Alexander <14028925+GitCaptain@users.noreply.github.com>
2021-11-23 03:14:39 +0300

    Initial logging fixes

--- RobinTheEngine/src/RobinTheEngine/Log.cpp
+++ RobinTheEngine/src/RobinTheEngine/Log.cpp
@@ -1,21 +1,46 @@
 #include "rtepch.h"
 #include "Log.h"
 
-
 namespace RTE
 {
-	std::shared_ptr<spdlog::logger> Log::s_CoreLogger = spdlog::stdout_color_mt("RTE");
-	std::shared_ptr<spdlog::logger> Log::s_ClientLogger = spdlog::stdout_color_mt("APP");
+    std::shared_ptr<spdlog::sinks::basic_file_sink_mt> Log::shared_file_sink =
+        std::make_shared<spdlog::sinks::basic_file_sink_mt>("Engine.log");
+    std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> Log::shared_console_sink =
+        std::make_shared<spdlog::sinks::stdout_color_sink_mt>("console.log");
 
-	void Log::Init()
-	{
-		
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger->set_level(spdlog::level::trace);
+    void Log::Init()
+    {
+        spdlog::set_pattern("%^[%T] %n: %v%$");
+        spdlog::set_level(spdlog::level::trace);
+    }
 
-		s_ClientLogger->set_level(spdlog::level::trace);
-		
-	}
+    std::shared_ptr<spdlog::logger> Log::GetLogger(std::string name)
+    {
+        auto logger = spdlog::get(std::string(name));
+        if (!logger) {
+            return CreateAndGetLogger(name);
+        }
+        return logger;
+    }
+
+    std::shared_ptr<spdlog::logger> Log::GetCoreLogger()
+    {
+        return GetLogger("RTE");
+    }
+
+    std::shared_ptr<spdlog::logger> Log::GetClientLogger()
+    {
+        return GetLogger("APP");
+    }
+
+    std::shared_ptr<spdlog::logger> Log::CreateAndGetLogger(std::string name)
+    {
+        auto newLoggerFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(name);
+        std::vector<spdlog::sink_ptr> sinks = { shared_console_sink, shared_file_sink, newLoggerFileSink };
+        auto newLogger = std::make_shared<spdlog::logger>(name, sinks.begin(), sinks.end());
+        spdlog::register_logger(newLogger);
+        return newLogger;
+    }
 
 }
 
--- RobinTheEngine/src/RobinTheEngine/Log.h
+++ RobinTheEngine/src/RobinTheEngine/Log.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/fmt/ostr.h"
 
@@ -10,14 +11,15 @@ namespace RTE
 	{
 	public:
 		static void Init();
-
-		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+		static std::shared_ptr<spdlog::logger> GetLogger(std::string name);
+		static std::shared_ptr<spdlog::logger> GetCoreLogger();
+		[[deprecated("Use GetLogger with meaningful name instead")]]
+		static std::shared_ptr<spdlog::logger> GetClientLogger();
 
 	private:
-		static std::shared_ptr<spdlog::logger> s_CoreLogger;
-		static std::shared_ptr<spdlog::logger> s_ClientLogger;
-
+		static std::shared_ptr<spdlog::logger> CreateAndGetLogger(std::string name);
+		static std::shared_ptr<spdlog::sinks::basic_file_sink_mt> shared_file_sink;
+		static std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> shared_console_sink;
 	};
 
 }
@@ -29,11 +31,11 @@ namespace RTE
 #define RTE_CORE_INFO(...)		::RTE::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define RTE_CORE_WARN(...)		::RTE::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define RTE_CORE_ERROR(...)		::RTE::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define RTE_CORE_FATAL(...)	::RTE::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define RTE_CORE_FATAL(...)		::RTE::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
-//core log macros
+//deprecated client log macros
 #define RTE_TRACE(...)		::RTE::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define RTE_INFO(...)		::RTE::Log::GetClientLogger()->info(__VA_ARGS__)
 #define RTE_WARN(...)		::RTE::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define RTE_ERROR(...)		::RTE::Log::GetClientLogger()->error(__VA_ARGS__)
-#define RTE_FATAL(...)	::RTE::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define RTE_FATAL(...)		::RTE::Log::GetClientLogger()->critical(__VA_ARGS__)
