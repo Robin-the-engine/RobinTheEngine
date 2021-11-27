28c104b9834b7bc74af167138b06b6729407038c
Alexander Sukhikh <alexsm.rf24@gmail.com>
2021-11-27 17:49:23 +0300

    Add logs directory. Set flush policy for logs

--- RobinTheEngine/src/RobinTheEngine/Log.cpp
+++ RobinTheEngine/src/RobinTheEngine/Log.cpp
@@ -3,15 +3,24 @@
 
 namespace RTE
 {
+    const static std::string logDir = "logs/";
+
     std::shared_ptr<spdlog::sinks::basic_file_sink_mt> Log::shared_file_sink =
-        std::make_shared<spdlog::sinks::basic_file_sink_mt>("Engine.log");
+        std::make_shared<spdlog::sinks::basic_file_sink_mt>(logDir + "Engine.log");
     std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> Log::shared_console_sink =
         std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
     void Log::Init()
     {
         spdlog::set_pattern("%^[%T] %n: %v%$");
+
+#ifndef DNDEBUG
         spdlog::set_level(spdlog::level::trace);
+        spdlog::flush_on(spdlog::level::trace);
+#else
+        spdlog::set_level(spdlog::level::info);
+        spdlog::flush_on(spdlog::level::err);
+#endif
     }
 
     std::shared_ptr<spdlog::logger> Log::GetLogger(std::string name)
@@ -35,7 +44,7 @@ namespace RTE
 
     std::shared_ptr<spdlog::logger> Log::CreateAndGetLogger(std::string name)
     {
-        auto newLoggerFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(name);
+        auto newLoggerFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logDir + name);
         std::vector<spdlog::sink_ptr> sinks = { shared_console_sink, shared_file_sink, newLoggerFileSink };
         auto newLogger = std::make_shared<spdlog::logger>(name, sinks.begin(), sinks.end());
         spdlog::register_logger(newLogger);
