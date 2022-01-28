5fea6df83ee2ab14e1eb02ef922cefd0e67d6387
Alexander Sukhikh <alexsm.rf24@gmail.com>
2022-01-28 08:06:25 +0300

    Update behaviour tree and implement AI example

---
+++ SandBox/Content/Scripts/ai.lua
@@ -0,0 +1,25 @@
+
+BlackBoard:set("bar", 50)
+
+function mul(a, b)
+    Log.info("mul: " .. a*b)
+    return TickResult.SUCCESS
+end
+
+function bark(times)
+    times = times or 1
+    for i=1,times do
+        Log.info("bark!")
+    end
+    return TickResult.RUNNING
+end
+
+function OnUpdate()
+    t = bark(3)
+    Log.info("res: " .. t)
+end
+
+function abort()
+    Log.info("\n\n\n\n\nACTION ABORTED\n\n\n\n\n")
+end
+
